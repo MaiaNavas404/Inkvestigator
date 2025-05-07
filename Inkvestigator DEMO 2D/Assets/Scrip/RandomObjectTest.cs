3b7892b46d6ca4d15fbc951c0a6d8fb442d7f1bf
AnitaHug <batnicolcho@gmail.com>
2025-05-08 00:11:54 +0200

    Multiple random object location
    Created a prefab to use as the trash. In the Unity inspector, you can adjust the amount of trash, as well as the minimum distance between them spawning.
    The script makes attempts to avoid overlap between objects positions.
    If failed, it issues a warning in the Unity console.
    To do: objects should not overlap with walls.

--- Inkvestigator DEMO 2D/Assets/Scrip/RandomObjectTest.cs
+++ Inkvestigator DEMO 2D/Assets/Scrip/RandomObjectTest.cs
@@ -1,20 +1,67 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomObjectTest : MonoBehaviour
 {
 
     Vector2 randomPosition;
-    [SerializeField] private float xRange = 3f;
-    [SerializeField] private float yRange = 3f;
+    [SerializeField] private GameObject _spawnObject; // can asign any prefab in the Unity inspector
+    [SerializeField] private int _spawnAmount = 8; // amount of objects that will spawn
+    [SerializeField] private float _xRange = 3f; // range for x axis
+    [SerializeField] private float _yRange = 3f; // range for y axis
+    [SerializeField] private float _minDistance = 1f;// min distance between objects
+
+    // Stores positions of all sucessfully spawned objects
+    private List<Vector2> _spawnedPositions = new List<Vector2>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float xPosition = Random.Range(0 - xRange, 0 + xRange);
-        float yPosition = Random.Range(0 - yRange, 0 + yRange);
+        int attempts = 0;
+        int maxAttempts = 1000; //stops any infinite loops
 
-        randomPosition = new Vector2(xPosition, yPosition);
-        transform.position = randomPosition;
+        for (int objectIndex = 0; objectIndex < _spawnAmount; objectIndex++)
+        {
+            Vector2 randomPosition;
+            bool positionFound = false;
+
+            while (!positionFound && attempts < maxAttempts)
+            {
+                float xPosition = Random.Range(0 - _xRange, 0 + _xRange);
+                float yPosition = Random.Range(0 - _yRange, 0 + _yRange);
+                randomPosition = new Vector2(xPosition, yPosition);
+                
+                bool overlap = false;
+
+                foreach (Vector2 existingPosition in _spawnedPositions)
+                {
+                    if (Vector2.Distance(randomPosition, existingPosition) < _minDistance)
+                    {
+                        overlap = true;
+                        break;
+                    }
+                }
+
+                if (!overlap)
+                {
+                    Instantiate(_spawnObject, randomPosition, Quaternion.identity);
+                    _spawnedPositions.Add(randomPosition);
+                    positionFound = true;
+                    
+                }
+
+                attempts++;
+            }
+
+            if (attempts >= maxAttempts)
+            {
+                Debug.LogWarning("Max attempts reached. Some objects might have not spawned.");
+                break;
+            }
+
+        }
+
+        //transform.position = randomPosition;
     }
 
     // Update is called once per frame
