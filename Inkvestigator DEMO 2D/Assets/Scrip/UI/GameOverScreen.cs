e4d0043975b378fd07a897ad4346f9d053a57de6
Iñaki <ignavilla.urbina@gmail.com>
2025-06-05 22:00:39 +0200

    Build 0.3

--- Inkvestigator DEMO 2D/Assets/Scrip/UI/GameOverScreen.cs
+++ Inkvestigator DEMO 2D/Assets/Scrip/UI/GameOverScreen.cs
@@ -3,8 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
+	public string menuSceneName; // e.g. "MainMenu"
+
 	public void RestartButton()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
+
+	public void HomeButton()
+	{
+		SceneManager.LoadScene(menuSceneName);
+	}
 }
+
